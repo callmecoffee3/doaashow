9b90e5ccef493c0040c5d2c083d00b5cba584244
callmecoffee3 <70238566+callmecoffee3@users.noreply.github.com>
2024-07-22 15:55:34 -0400

    Update doashow scial.bat

--- doashow scial.bat
+++ doashow scial.bat
@@ -1,5 +1,6 @@
 ```
 @echo off
+:my_doashow_social
 
 :main_menu
 cls
@@ -26,15 +27,93 @@ if %choice%==0 goto :user_menu
 if %choice%==1 goto :on_the_burner
 if %choice%==2 goto :create_menu
 if %choice%==3 goto :discover_menu
-if %choice%==9 goto :tv_menu
+if %choice%==4 goto :community_menu
+if %choice%==5 goto :products_menu
+if %choice%==6 goto :settings_menu
+if %choice%==7 goto :help_menu
+if %choice%==8 goto :write_a_messages_menu
+if %choice%==9 goto:tv_menu
 if %choice%==10 goto :radio_menu
 if %choice%==11 goto :podcast_menu
 if %choice%==12 goto :list_menu
 if %choice%==13 goto :business_menu
-if %choice%== 16 goto :quit
-if %choice%==6 goto :main_menu
+if %choice%==14 goto :community_menu
 if %choice%==14 goto :pages_menu
 if %choice%==15 goto :groups_menu
+if %choice%==16 goto :quit
+
+:products_menu
+cls
+echo 1. products
+echo 2. users
+echo 3. discover
+echo 4. add products
+echo 5. org
+echo 6. type
+echo 7. sell
+echo 8. buy 
+echo 9. trade
+echo . Back to Main Menu
+set /p choice=Enter your choice: 
+
+:Discover_menu
+cls
+echo 1. dicsover
+echo 2. friends
+echo 3. pages
+echo 4. groups
+echo 5. users
+echo 6. add
+echo 7. remove
+echo 8. like
+echo 9. save
+echo . Back to Main Menu
+set /p choice=Enter your choice: 
+
+
+:products_menu
+cls
+echo 1. products
+echo 2. users
+echo 3. discover
+echo 4. add products
+echo 5. org
+echo 6. type
+echo 7. sell
+echo 8. buy 
+echo 9. trade
+echo . Back to Main Menu
+set /p choice=Enter your choice: 
+
+:Help_menu
+cls
+echo 1. feedback
+echo 2. sumbit
+echo 3. discover
+echo 4. trouble
+echo 5. community
+echo 6. type
+echo 7. sell
+echo 8. buy 
+echo 9. trade
+echo . Back to Main Menu
+set /p choice=Enter your choice: 
+
+
+:Settings_menu
+cls
+echo 1. groups
+echo 2. users
+echo 3. discover
+echo 4. add groups
+echo 5. org
+echo 6. type
+echo 7. sell
+echo 8. buy 
+echo 9. trade
+echo . Back to Main Menu
+set /p choice=Enter your choice: 
+
 
 :groups_menu
 cls
