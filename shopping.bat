1f3c00b05b4d7a8897bde9909011ce874ac62024
callmecoffee3 <70238566+callmecoffee3@users.noreply.github.com>
2024-07-21 20:45:49 -0400

    Update shopping.bat

---
+++ shopping.bat
@@ -0,0 +1,96 @@
+Shopping list revised
+
+Here is the revised shopping list with a user interface in the command prompt, including a main menu selection:
+
+```
+**My Menu **
+
+[1] My Shopping List
+[2] Select Shopping 
+[3] Exit
+
+**My Shopping List:**
+
+1. Eggs (18 ct)
+2. Bacon (pk)
+3. Chicken (legs)
+4. Popcorn (pk)
+5. Syrup (bottle)
+6. Sweetener (bottle)
+7. Milk 2% (gal)
+8. Water (bottles)
+9. Spaghetti sauce (jar)
+10. Spaghetti (pk)
+11. Meat (pk)
+12. Hot dog (24 ct)
+13. Ricotta cheese (pk)
+14. Cheese sticks (pk)
+15. Mountain dew (bottle)
+16. Sliced cheese (bag)
+17. Ramen shrimp noodles (box)
+18. Pizza (microwave)
+19. Pepperoni (bag)
+20. Paper towels (pk)
+21. Paper plates (pk)
+22. Cheddar cheese (sliced)
+23. Seafood dinners (microwave)
+24. Condensed milk (can)
+25. C batteries (pk)
+26. Apple cider (powdered)
+27. Maple syrup (jug)
+28. Onion powder (bottle)
+29. Garlic powder (bottle)
+30. Index cards (pk)
+31. Chicken broth (box)
+32. Shampoo
+
+**Select Shopping**
+
+1. Eggs (18 ct)
+2. Bacon (pk)
+3. Chicken (legs)
+4. Popcorn (pk)
+5. Syrup (bottle)
+6. Sweetener (bottle)
+7. Milk 2% (gal)
+8. Water (bottles)
+9. Spaghetti sauce (jar)
+10. Spaghetti (pk)
+11. Meat (pk)
+12. Hot dog (24 ct)
+13. Ricotta cheese (pk)
+14. Cheese sticks (pk)
+15. Mountain dew (bottle)
+16. Sliced cheese (bag)
+17. Ramen shrimp noodles (box)
+18. Pizza (microwave)
+19. Pepperoni (bag)
+20. Paper towels (pk)
+21. Paper plates (pk)
+22. Cheddar cheese (sliced)
+23. Seafood dinners (microwave)
+24. Condensed milk (can)
+25. C batteries (pk)
+26. Apple cider (powdered)
+27. Maple syrup (jug)
+28. Onion powder (bottle)
+29. Garlic powder (bottle)
+30. Index cards (pk)
+31. Chicken broth (box)
+32. Shampoo
+
+**Commands:**
+
+C:\Shopping>select [option number]
+C:\Shopping>add [item name]
+C:\Shopping>remove [item number]
+C:\Shopping>save
+C:\Shopping>load
+C:\Shopping>list
+
+Note: Type 'select' to select an option, 'add' to add an item, 'remove' to remove an item, 'save' to save the list, 'load' to load a saved list, and 'I list' to display the list.
+
+C:\Shopping>_
+```
+
+Please enter a command:
